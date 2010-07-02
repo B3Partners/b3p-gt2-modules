a30f87d304d067e4ab1baae5c886f1eecaa6b270
Erik van de Pol <erikvandepol@b3partners.nl>
2010-07-02 13:14:01 +0000

    Geotools update naar trunk (2.6.4 op het moment)

--- src/nl/b3p/geotools/data/csv/CSVDataStoreFactory.java
+++ src/nl/b3p/geotools/data/csv/CSVDataStoreFactory.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 import org.geotools.data.DataStore;
 import org.geotools.data.DataStoreFactorySpi;
+import org.geotools.data.FileDataStore;
 import org.geotools.data.FileDataStoreFactorySpi;
 import org.geotools.referencing.CRS;
 import org.opengis.referencing.FactoryException;
@@ -100,7 +101,7 @@ public class CSVDataStoreFactory implements FileDataStoreFactorySpi {
         return CSVDataStore.getURLTypeName(url);
     }
 
-    public DataStore createDataStore(URL url) throws IOException {
+    public FileDataStore createDataStore(URL url) throws IOException {
         Map params = new HashMap();
         params.put(PARAM_URL.key, url);
 
@@ -112,7 +113,7 @@ public class CSVDataStoreFactory implements FileDataStoreFactorySpi {
         }
     }
 
-    public DataStore createDataStore(Map params) throws IOException {
+    public FileDataStore createDataStore(Map params) throws IOException {
         if (!canProcess(params)) {
             throw new FileNotFoundException("Unable to process CSV file: " + params);
         }
