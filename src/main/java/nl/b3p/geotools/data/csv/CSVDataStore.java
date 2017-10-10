4911d09d42d20541eee6fef3bb0e5038f7c8d560
Mark Prins <markprins@b3partners.nl>
2017-10-10 16:53:08 +0200

    Upgrade naar GeoTools 18

--- src/main/java/nl/b3p/geotools/data/csv/CSVDataStore.java
+++ src/main/java/nl/b3p/geotools/data/csv/CSVDataStore.java
@@ -1,18 +1,33 @@
 package nl.b3p.geotools.data.csv;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.geotools.data.AbstractFileDataStore;
+import org.geotools.data.DefaultServiceInfo;
 import org.geotools.data.FeatureReader;
+import org.geotools.data.FeatureWriter;
+import org.geotools.data.FileDataStore;
+import org.geotools.data.LockingManager;
 import org.geotools.data.Query;
+import org.geotools.data.ServiceInfo;
+import org.geotools.data.Transaction;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.feature.NameImpl;
+import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.Name;
+import org.opengis.filter.Filter;
 
 /**
  * @author Gertjan Al, B3Partners
+ * @author mprins
  */
-public class CSVDataStore extends AbstractFileDataStore {
+public class CSVDataStore implements FileDataStore {
 
     private static final Log log = LogFactory.getLog(CSVDataStore.class);
     private URL url;
@@ -55,6 +70,17 @@ public class CSVDataStore extends AbstractFileDataStore {
         }
     }
 
+    @Override
+    public List<Name> getNames() throws IOException {
+        return Arrays.asList((Name) new NameImpl(getSchema().getTypeName()));
+    }
+
+    @Override
+    public SimpleFeatureType getSchema(Name name) throws IOException {
+        return getSchema();
+    }
+
+    @Override
     public SimpleFeatureType getSchema(String typeName) throws IOException {
         /* only one type */
         return getSchema();
@@ -70,6 +96,14 @@ public class CSVDataStore extends AbstractFileDataStore {
         return getFeatureReader();
     }
 
+    /**
+     * filtering is not supported.
+     */
+    @Override
+    public FeatureReader<SimpleFeatureType, SimpleFeature> getFeatureReader(Query query, Transaction t) throws IOException {
+        return getFeatureReader();
+    }
+
     @Override
     public FeatureReader getFeatureReader() throws IOException {
         if (featureReader == null) { //|| !featureReader.hasNext()) {
@@ -82,20 +116,125 @@ public class CSVDataStore extends AbstractFileDataStore {
         return featureReader;
     }
 
-    @Override
     protected int getCount(Query query) throws IOException {
         //if (query.equals(Query.ALL)) {
-            // always returns the count of all features in the csv-file.
-            getFeatureReader();
-            int count = 0;
-            while (featureReader.hasNext()) {
-                featureReader.next(); // not necessary, but more future-proof.
-                count++;
-            }
-            featureReader.close();
-            featureReader = null;
-            return count;
+        // always returns the count of all features in the csv-file.
+        getFeatureReader();
+        int count = 0;
+        while (featureReader.hasNext()) {
+            featureReader.next(); // not necessary, but more future-proof.
+            count++;
+        }
+        featureReader.close();
+        featureReader = null;
+        return count;
         //}
         //return super.getCount(query);
     }
+
+    protected List<Name> createTypeNames() throws IOException {
+        Name typeName = new NameImpl(this.typename);
+        return Collections.singletonList(typeName);
+    }
+
+
+    @Override
+    public SimpleFeatureSource getFeatureSource() throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public SimpleFeatureSource getFeatureSource(String string) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public SimpleFeatureSource getFeatureSource(Name name) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void createSchema(SimpleFeatureType t) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public void updateSchema(SimpleFeatureType sft) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public void updateSchema(Name name, SimpleFeatureType t) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public void updateSchema(String string, SimpleFeatureType sft) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public void removeSchema(Name name) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+    @Override
+    public void removeSchema(String string) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public FeatureWriter<SimpleFeatureType, SimpleFeature> getFeatureWriter(Filter filter, Transaction t) throws IOException {
+        throw new UnsupportedOperationException("Functie niet ondersteund voor alleen-lezen databron.");
+    }
+
+    @Override
+    public FeatureWriter<SimpleFeatureType, SimpleFeature> getFeatureWriter(Transaction t) throws IOException {
+        throw new UnsupportedOperationException("Functie niet ondersteund voor alleen-lezen databron.");
+    }
+
+    @Override
+    public FeatureWriter<SimpleFeatureType, SimpleFeature> getFeatureWriterAppend(Transaction t) throws IOException {
+        throw new UnsupportedOperationException("Functie niet ondersteund voor alleen-lezen databron.");
+    }
+
+    @Override
+    public FeatureWriter<SimpleFeatureType, SimpleFeature> getFeatureWriter(String string, Filter filter, Transaction t) throws IOException {
+        throw new UnsupportedOperationException("Functie niet ondersteund voor alleen-lezen databron.");
+    }
+
+    @Override
+    public FeatureWriter<SimpleFeatureType, SimpleFeature> getFeatureWriter(String string, Transaction t) throws IOException {
+        throw new UnsupportedOperationException("Functie niet ondersteund voor alleen-lezen databron.");
+    }
+
+    @Override
+    public FeatureWriter<SimpleFeatureType, SimpleFeature> getFeatureWriterAppend(String string, Transaction t) throws IOException {
+        throw new UnsupportedOperationException("Functie niet ondersteund voor alleen-lezen databron.");
+    }
+
+    @Override
+    public LockingManager getLockingManager() {
+        throw new UnsupportedOperationException("Functie niet ondersteund voor alleen-lezen databron.");
+    }
+
+    @Override
+    public ServiceInfo getInfo() {
+        DefaultServiceInfo serviceInfo = new DefaultServiceInfo();
+        serviceInfo.setTitle("CSV DataStore");
+        try {
+            serviceInfo.setSource(this.url.toURI());
+        } catch (URISyntaxException ex) {
+
+        }
+        return serviceInfo;
+    }
+
+    @Override
+    public void dispose() {
+        try {
+            this.featureReader.close();
+        } catch (IOException ex) {
+            // ignore
+        }
+        this.featureReader = null;
+    }
+
 }
