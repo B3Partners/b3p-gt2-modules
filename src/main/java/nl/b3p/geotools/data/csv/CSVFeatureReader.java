2a49d4ad68b688be99e21737c8e74545a831602f
Mark Prins <markprins@b3partners.nl>
2019-02-08 10:02:47 +0100

    upgrade naar geotools 20.x

--- src/main/java/nl/b3p/geotools/data/csv/CSVFeatureReader.java
+++ src/main/java/nl/b3p/geotools/data/csv/CSVFeatureReader.java
@@ -3,9 +3,9 @@
  */
 package nl.b3p.geotools.data.csv;
 
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
